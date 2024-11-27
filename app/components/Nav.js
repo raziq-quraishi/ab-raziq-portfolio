8ab2e295d4615b72c276d48e666958501b2d5497
Quraishi <abdulraziqquraishi97@gmail.com>
2024-11-27 16:01:43 +0430

    Working on Navbar Responsive

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -2,8 +2,11 @@
 import { useState } from "react";
 import Link from "next/link";
 import { CiDark, CiLight } from "react-icons/ci";
+import { BsMoonStars } from "react-icons/bs";
+import { IoMenu, IoClose } from "react-icons/io5";
 const Nav = () => {
   const [theme, setTheme] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
   const links = [
     {
       label: "Home",
@@ -26,20 +29,41 @@ const Nav = () => {
   const changeTheme = () => {
     setTheme(!theme)
   }
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
   return (
-    <header className="bg-thirdGreen  flex items-center gap-5 px-10">
-      <Link href="#"><h1 className="text-white hover:text-textColor text-2xl font-bold mr-auto text-uppercase uppercase">Quraishi</h1></Link>
-      <nav className="flex items-center justify-between gap-5 max-w-screen-lg mx-auto py-3">
-        {
-          links.map((link, index) => {
-            return (<Link className="text-white hover:text-textColor font-semibold " key={index} href={link.href}>{link.label}</Link>)
-          }
-          )
-        }
-      </nav>
-      <button className="text-white hover:text-textColor">Resume</button>
-      <button onClick={changeTheme} className="text-white hover:text-textColor">{theme ? <CiLight className="text-3xl" /> : <CiDark className="text-3xl" />}</button>
-    </header>
+
+
+    <nav className=" bg-white border-gray-200 dark:bg-thirdGreen">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <h1 className="text-white hover:text-textColor text-2xl font-bold mr-auto text-uppercase uppercase">Quraishi</h1>
+        </Link>
+        {/* Dark Mode Button When Screen is small */}
+        <button onClick={changeTheme} className="block md:hidden text-white hover:text-textColor">{theme ? <CiLight className="text-xl" /> : <BsMoonStars className="text-xl" />}</button>
+
+        <button onClick={toggleMenu} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden  focus:outline-none  dark:text-gray-50  " aria-controls="navbar-default" aria-expanded="false">
+
+          {isOpen ? <IoClose className="text-2xl" /> : <IoMenu className="text-2xl" />}
+        </button>
+        <div className={` w-full  ${isOpen ? 'block' : 'hidden'} md:block md:w-auto`} id="navbar-default">
+          <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0   dark:bg-thirdGreen md:dark:bg-thirdGreen ">
+            {links.map((link, index) => <li key={index}>
+              <Link href={link.href} className="block py-2 px-3 text-white  rounded md:bg-transparent  md:p-0 dark:text-white " aria-current="page">{link.label}</Link>
+            </li>)}
+            <button className="block md:hidden text-white hover:text-textColor">Resume</button>
+          </ul>
+        </div>
+
+        <div className="hidden md:flex items-center gap-5">
+          <button className="text-white hover:text-textColor">Resume</button>
+          <button onClick={changeTheme} className="text-white hover:text-textColor">{theme ? <CiLight className="text-xl" /> : <BsMoonStars className="text-xl" />}</button>
+
+        </div>
+      </div>
+    </nav>
+
   );
 };
 
