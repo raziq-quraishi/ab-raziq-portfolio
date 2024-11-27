96d056533f146477271ab81c21c40e3d1c7965fc
Quraishi <abdulraziqquraishi97@gmail.com>
2024-11-27 16:26:39 +0430

    Working on Dark Mode and Light Mode...

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -35,7 +35,7 @@ const Nav = () => {
   return (
 
 
-    <nav className=" bg-white border-gray-200 dark:bg-thirdGreen">
+    <nav className=" bg-thirdGreen  dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <h1 className="text-white hover:text-textColor text-2xl font-bold mr-auto text-uppercase uppercase">Quraishi</h1>
